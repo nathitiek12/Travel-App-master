435a67b118f387b62ebab0ea36c6de518d95afc3
Raliciel <33251984+Raliciel@users.noreply.github.com>
2023-11-29 21:19:36 +0700

    Add favorite function

--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {colors} from '../constants/theme';
+import MainHeader from '../components/shared/MainHeader';
 
 const FavoriteScreen = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Favorite</Text>
+    <View style={styles.container}>
+      <MainHeader title="Favorite" />
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <ShowFavorite />
+      </View>
     </View>
   );
 };
+
+const ShowFavorite = props => {
+  const [fav, Setfave] = useState(null);
+  if (fav == null) {
+    return <Text>You do not have any Favorite</Text>;
+  } else {
+    return <Text>You have favorite</Text>;
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.light,
+  },
+});
 
 export default FavoriteScreen;
